70e163c3d3387a95a33971ee33f7b9046e6da792
Elvis <tobennaonyike@gmail.com>
2026-08-10 16:27:07 +0100

    feat: setup redux and product api

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,28 @@
-import './App.css'
+import { useGetProductsQuery } from './services/productsApi';
 
 function App() {
+  const { data, isLoading, isError } = useGetProductsQuery();
 
+  if (isLoading) {
+    return <p>Loading products...</p>;
+  }
+
+  if (isError) {
+    return <p>Failed to load products.</p>;
+  }
 
   return (
-   <div>
-    <h1>hello world</h1>
-   </div>
-  )
+    <main>
+      <h1>Products</h1>
+
+      {data?.products.map((product) => (
+        <div key={product.id}>
+          <h2>{product.title}</h2>
+          <p>${product.price}</p>
+        </div>
+      ))}
+    </main>
+  );
 }
 
-export default App
+export default App;
